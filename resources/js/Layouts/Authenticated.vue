394fb442228e91089682a8ab9b905f28281a6093
Marvin Abante <abante.marvin@yahoo.com>
2022-10-18 23:10:19 +0800

    Vue3 and Laravel 9 CRUD done with authentication.

---
+++ resources/js/Layouts/Authenticated.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import BreezeNavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+
+const showingNavigationDropdown = ref(false);
+</script>
+<!-- Navigation Links -->
+<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+
+    <BreezeNavLink
+        :href="route('dashboard')"
+        :active="route().current('dashboard')"
+    >
+        Dashboard
+    </BreezeNavLink>
+
+    <BreezeNavLink :href="route('posts.index')" :active="route().current('posts.index')">
+        Posts
+    </BreezeNavLink>
+
+  </div>
